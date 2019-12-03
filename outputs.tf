cf1e8bf7ee322decda4cc4f7cba1ee3d8892a0ad
ish-xyz <ishamaraia@gmail.com>
2019-12-03 22:50:41 +0000

    Add switch for pre-generated CAs, add description on vars

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "key" {
 output "cert" {
    value = tls_locally_signed_cert.local.cert_pem
 }
+
+output "ca_key" {
+   value = tls_private_key.ca.private_key_pem
+}
+
+output "ca_cert" {
+   value = tls_self_signed_cert.ca.cert_pem
+}
